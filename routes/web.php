c49112d2f1a0658df3c105c3f3c1cede1691c1ee
Eskuero42 <jhosspr0401@gmail.com>
2024-04-18 19:45:53 -0400

    evaluacion

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\CursoController;
 
 
 Route::get('/', function () {
-    return view('estateagency.pages.blog-grid');
+    return view('InterfazPractica.interfaz');
 });
 
 Route ::get('/home', function (){
